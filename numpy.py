1d369bea44ceb558c5edc0bf9afe276e8082f44a
Diego Martín López <2021352@iesluisbraille.es>
2022-02-03 10:14:26 +0100

    first commit

---
+++ numpy.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 09:03:55 2022
+
+@author: DiegoMartínLópez
+"""
+
+
+import pandas as pd
+import numpy as np
+
+
+userHeader = ['user_id', 'gender', 'age', 'ocupation', 'zip']
+users = pd.read_table('d:/usuarios/Users/DiegoMartínLópez/Documents/Programacion/Base de datos movies Programacion/ml-1m/users.dat', engine='python', sep='::',
+header=None, names=userHeader)
+movieHeader = ['movie_id', 'title', 'genders']
+movies = pd.read_table('d:/usuarios/Users/DiegoMartínLópez/Documents/Programacion/Base de datos movies Programacion/ml-1m/movies.dat', engine='python', sep='::'
+, header=None, names=movieHeader, encoding="latin1")
+ratingHeader = ['user_id', 'movie_id', 'rating', 'timestamp']
+ratings = pd.read_table('d:/usuarios/Users/DiegoMartínLópez/Documents/Programacion/Base de datos movies Programacion/ml-1m/ratings.dat', engine='python', sep='::', header=None, names=ratingHeader)
+# Merge data
+mergeRatings = pd.merge(pd.merge(users, ratings), movies)
+
+moviesUserZip = mergeRatings.copy()
+moviesUserZip2 = moviesUserZip.groupby(['zip', 'user_id', 'title'])['rating'].agg(
+        SUM=np.sum, COUNT=np.size, AVG=np.mean, myAVG=lambda x: x.sum() / float(x.count()))
+
